6760b6387708b58ddc0123c6aeae75a9e9887524
Kartikeya <kartikeyasrivastava861@gmail.com>
2021-08-07 13:32:04 +0530

    updated intro screen, maze is divided
    cells division of maze is complete

--- app_class.py
+++ app_class.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 from settings import *
+import os
 
 pygame.init()
 vec = pygame.math.Vector2
@@ -14,7 +15,9 @@ class App:
         self.running = True
         self.state = 'start'
         self.run()
-    
+
+
+        self.load()
     def run(self):
  #       print('call run')
         while self.running:
@@ -22,22 +25,46 @@ class App:
                 self.start_events()
                 self.start_update()
                 self.start_draw()
+            elif self.state =='playing':
+                self.playing_events()
+                self.playing_update()
+                self.playing_draw()
             else:
-                pass
+                self.running = False
             self.clock.tick(FPS)
         pygame.quit()
         sys.exit()
 
 
 ###################SUPPORT DEFS#################
-    def draw_text(self, text, screen, size, pos, color, font_name):
-#        print('text_called')
-        font = pygame.font.SysFont(font_name, size)
+
+# CENTRE CALL IN GENERAL BY SUBTRACTING THE POS - SIZE 
+# POSITION CANNOT BE A TUPLE BECAUSE YOU CANT ACCESS INDEX, TUPLES SUCC ASS
+    def draw_text(self, text, screen, size, pos, color, font_name, centred = False): 
+ #       print(font_name)
+        font = pygame.font.Font(font_name, 16)
         printing= font.render(text, False, color)
         text_size = printing.get_size()
-        pos[0] = pos[0] - text_size[0]//2
-        pos[1] = pos[1] - text_size[1]//2
+        if centred == True:
+            pos[0] = pos[0] - text_size[0]//2
+            pos[1] = pos[1] - text_size[1]//2
+        
         screen.blit(printing, pos)
+
+    def load(self):
+        self.background = pygame.image.load('maze.png')
+        self.background = pygame.transform.scale(self.background, (WIDTH, HEIGHT))
+    
+    def draw_grid(self):
+        for x in range(WIDTH//cell_width):
+            pygame.draw.line(self.screen,GREY,(x*cell_width,0),(x*cell_width, HEIGHT))
+        for y in range(HEIGHT//cell_height):
+            pygame.draw.line(self.screen, GREY,(0, y*cell_height), (WIDTH, y*cell_height))
+
+
+
+
+
 
 ###################INTRO DEFS #################
 
@@ -55,5 +82,30 @@ class App:
     def start_draw(self):
         self.screen.fill(BLACK)
 #        print('draw call')
-        self.draw_text('PUSH SPACE BAR',self.screen, START_TEXT_SIZE,[WIDTH//2,HEIGHT//2], (170,132,158),START_FONT)
+        self.draw_text('PRESS SPACE BAR',self.screen, 38,[WIDTH//2,HEIGHT//2-50], (243,58,106),START_FONT, centred= True)
+        self.draw_text('1 PLAYER MODE ONLY', self.screen, 22, [WIDTH//2, HEIGHT//2 + 50], (44, 168, 198), START_FONT, centred=True)
+        self.draw_text('© 2021 Kartikeya Srivastava', self.screen, 22, [WIDTH//2, HEIGHT//2 + 100], (255, 255, 0), START_FONT, centred = True)
+        self.draw_text('HIGH SCORE', self.screen, 12, [5,0], (0, 255, 0), START_FONT)
+        self.draw_text('1P', self.screen, 12, [410, 0], (0, 255, 0), START_FONT)
+
+                       
         pygame.display.update()
+
+###################PLAYING DEFS #################
+
+    def playing_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+
+
+    def playing_update(self):
+        pass
+
+    def playing_draw(self):
+        self.load()
+        self.screen.blit(self.background,(0,0))
+#        print('called')
+        self.draw_grid()
+        pygame.display.update()
+ 
--- settings.py
+++ settings.py
@@ -1,11 +1,15 @@
 #height and width of the program
-WIDTH,HEIGHT = 448,596
+WIDTH,HEIGHT = 560,620
+cell_width = WIDTH//28
+cell_height = HEIGHT//30
 #FRAMES PS
 FPS = 60
 #color default
 BLACK = (0,0,0)
+RED = (255,0,0)
+GREY = (107,107,107)
 #START SCREEN SETTINGS
-START_TEXT_SIZE = 16
-START_FONT = 'arial black'
+START_TEXT_SIZE = 32
+START_FONT = 'PressStart2P-vaV7.ttf'
 
 print('setting call')
