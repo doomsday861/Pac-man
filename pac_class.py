b59ea5586ce8599813a26f12ecc6bbeaa8a9d94a
Kartikeya Srivastava <kartikeyasrivastava861@gmail.com>
2021-08-08 18:23:58 +0530

    WALLS+MOVEMENT WORK
    walls and movememnt work nowwwww
     less gooo

--- pac_class.py
+++ pac_class.py
@@ -10,12 +10,15 @@ class Pac:
         self.direction = vec(1,0)
         self.stored_direction = None
         self.able_to_move = True
+        self.speed = 2
  
     def update(self):
+        if self.able_to_move:
+            self.pix_pos += self.direction*self.speed
         if self.time_to_move():
             if self.stored_direction != None:
                 self.direction = self.stored_direction
-            
+
             self.able_to_move = self.can_move()
 
  
